2092fa3753fc634fe2451d16276c734ba726c41c
Marc <marc@shawbe.com>
2026-06-17 21:54:49 +0200

    fix: use DATABASE_URL for production

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,19 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "postgres",
-  password: "TON_MOT_DE_PASSE",
-  port: 5432
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }
+      }
+    : {
+        user: "postgres",
+        host: "localhost",
+        database: "postgres",
+        password: "TON_MOT_DE_PASSE",
+        port: 5432
+      }
+);
 
 export default pool;
